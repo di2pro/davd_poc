accf58826e328a30121d067805f4b5b5fabb6543
Dmitriy Uvarov <dimmond.pro@gmail.com>
2019-12-18 17:06:49 +0300

    updating models, adding option to add users and organizations

--- entity/models.py
+++ entity/models.py
@@ -5,12 +5,12 @@ from django.contrib.postgres.fields.jsonb import JSONField
 class Entity(models.Model):
     is_enabled = models.BooleanField(default=True, null=False, blank=False)
     name = models.CharField(null=False, blank=False, max_length=120)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     disabled_at = models.DateTimeField(null=True, blank=True)
     organization = models.ForeignKey('organization.Organization', related_name='entities', on_delete=models.CASCADE)
 
 
 class EntityData(models.Model):
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     data = JSONField()
     entity = models.ForeignKey('Entity', related_name='data', on_delete=models.CASCADE)
--- organization/models.py
+++ organization/models.py
@@ -1,16 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 
 class Organization(models.Model):
     is_enabled = models.BooleanField(default=True, null=False)
     name = models.CharField(null=False, blank=False, max_length=120)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     disabled_at = models.DateTimeField(null=True, blank=True)
 
 
 class User(AbstractUser):
     is_enabled = models.BooleanField(default=True, null=False)
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     disabled_at = models.DateTimeField(null=True, blank=True)
     organization = models.ForeignKey('Organization', on_delete=models.CASCADE, related_name='members')
+
+
+@receiver(pre_save, sender=User)
+def create_user_organization(sender, instance, *args, **kwargs):
+    organization = Organization.objects.create(
+        name=f"{instance.first_name}'s organization"
+    )
+    instance.organization = organization
